cf365f49506fcb1964207ae42db7ea6367412896
Shekhar Kulkarni <sgkulkarni1997@gmail.com>
2023-01-18 19:44:32 +0530

    Endpoints added for CRUDL operations

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Configuring flask application
 
 Start virtual environment
 ~~~~~~~~~~~~~~~~~~~~~~~~~
------
+
 1. Install python in machine
 2. Go to VSCode terminal
 3. Enter command as `python -m venv .venv`
@@ -14,14 +14,36 @@ Start virtual environment
 7. Bin the current terminal 
 8. Open a new terminal 
 9. The project will start with venv. e.g. Path in terminal will have "(.venv)" as start
------
+
 
 
 Run the flask app
 ~~~~~~~~~~~~~~~~~
-----
+
 1. Check if virtual environment is active. If not refer to above steps to start venv
 2. Go to flask_store folder.
 3. Run  `pip install -r requirements.txt` in terminal.
 4. Start flask app with `flask run` this command.
-----
+
+
+Docker image build and run
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+1. Check Dockerfile
+2. Run command `docker build -t <image_name> .`
+3. To run image enter `docker run -dp 5000:5000 <image_name>`
+
+Run docker container
+
+1. Hot run the docker container.
+
+Windows:
+        `docker run -dp 5000:5000 -w /app -v "/c/Documents/yourproject:/app" flask-smorest-api`
+        
+Linux, MacOS:
+        `docker run -dp 5000:5000 -w /app -v "$(pwd):/app" flask-smorest-api`
+
+2. Build a docker image
+    `docker build -t flask-smorest-api .`
+3. Run container
+    `docker run -dp 5000:5000 flask-smorest-api`
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from flask_smorest import abort
 from db import stores, items
 import uuid
 
@@ -7,13 +8,21 @@ app = Flask(__name__)
 
 # Get all stores data
 @app.get("/store")  # http://127.0.0.1:5000/store
-def get_store():
+def get__all_store():
     return {"stores": list(stores.values())}
+
+# Get all items
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
 
 # Create a new store
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, "Store already exist ")
     store_id = uuid.uuid4().hex
     new_store = {**store_data, "store_id": store_id}
     stores[store_id] = new_store
@@ -23,18 +32,21 @@ def create_store():
 @app.post("/item")
 def create_item(): 
     item_data = request.get_json()
+    if ("name" not in item_data 
+        or "price" not in item_data
+        or "store_id" not in item_data):
+            abort(400,
+                "Bad request, include all json data")
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message= "Store not found.")
+    for item in items.values():
+        if item_data["name"] == item["name"] and item_data["price"] == item["price"]:
+            abort(400, "Item already exist in same store")
     item_id = uuid.uuid4().hex
     new_item = {**item_data, "item_id": item_id}
     items[item_id] = new_item
     return new_item
 
-
-# Get all items
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
 
 # Get details for a particular store
 @app.get("/store/<string:store_id>")
@@ -42,12 +54,55 @@ def get_store_data(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message= "Store not found.")
 
-# Get items in store
+# Get item 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return {"message": "Item not found"}, 404
+       abort(404, message= "Item not found.")
+
+# Update existing Store
+@app.put("/store/<string:store_id>")
+def update_store(store_id):
+    store_data = request.get_json()
+    try:
+        stores[store_id] |= store_data
+        return {"message": "Store Updated"}
+    except KeyError:
+        abort(404, message="Store not found")
+
+
+# Update existing Item
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name"  not in item_data:
+        abort(400, message = "Bad request. Enter price and name")
+    try:
+        items[item_id] = item_data
+        return {"message": "Items updated"}
+    except KeyError:
+        abort(404, message="Item not found")
+
+
+# Delete a store
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted"}
+    except KeyError:
+        abort(404, message="Store not found")
+
+
+# Delete item
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}
+    except KeyError: 
+        abort(404, message="Item not found")
