3230fa7d15a89d0aaedc8ad2bcb1df0612f5b0d8
Shekhar Kulkarni <60649014+kulkarnisg@users.noreply.github.com>
2023-11-30 16:12:41 +0530

    Update item.py

--- models/item.py
+++ models/item.py
@@ -4,7 +4,7 @@ class ItemModel(db.Model):
     # This is the name of table to refer
     __tablename__ = "items"
 
-    id = db.Column(db.Integer, primary_key= True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
     store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False)
