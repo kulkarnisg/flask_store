b31c6d2e2ffe609493e4ba3b577dd6cea7db415f
Shekhar Kulkarni <60649014+kulkarnisg@users.noreply.github.com>
2023-11-30 16:12:14 +0530

    Update store.py

--- models/store.py
+++ models/store.py
@@ -4,6 +4,6 @@ class StoreModel(db.Model):
     # This is the name of table to refer
     __tablename__ = "stores"
 
-    id = db.Column(db.Integer, primary_key= True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     items = db.relationship("ItemModel", back_populates="store", lazy="dynamic", cascade="all, delete")
